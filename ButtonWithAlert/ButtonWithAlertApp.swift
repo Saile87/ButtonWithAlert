242b11f72e0d44b7b365279d9fd4fa1355a0b47f
Elias Breitenbach <elias.b@me.com>
2023-03-15 21:22:42 +0100

    Pushed the project to github

---
+++ ButtonWithAlert/ButtonWithAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonWithAlertApp.swift
+//  ButtonWithAlert
+//
+//  Created by Elias Breitenbach on 03.03.23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonWithAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonWithAlert/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ButtonWithAlert
+//
+//  Created by Elias Breitenbach on 03.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Button {
+                Text("Press")
+            }
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
